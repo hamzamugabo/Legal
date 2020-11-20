138e038c753ac3b8d608882e564ac9119244f3e0
hamzamugabo <hamzamugabo3@gmail.com>
2020-11-20 18:16:00 +0300

    Updates

---
+++ precache-manifest.0b2315544e50b5912b97f2bb7f208cca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cfdc3c8f12e06bf9eb27522c98aa3f7",
+    "url": "/legal/index.html"
+  },
+  {
+    "revision": "28e4bed9998fe789cd7c",
+    "url": "/legal/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "87b1348ea7edd777d47b",
+    "url": "/legal/static/css/main.bad6c48e.chunk.css"
+  },
+  {
+    "revision": "28e4bed9998fe789cd7c",
+    "url": "/legal/static/js/2.0c7f7a2a.chunk.js"
+  },
+  {
+    "revision": "df2659f20723d9e518ad965c595b69ee",
+    "url": "/legal/static/js/2.0c7f7a2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87b1348ea7edd777d47b",
+    "url": "/legal/static/js/main.e1d69505.chunk.js"
+  },
+  {
+    "revision": "298fab37e4329a3c5c5b",
+    "url": "/legal/static/js/runtime-main.5d7cb8c0.js"
+  }
+]);
